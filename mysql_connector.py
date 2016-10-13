d3dd82ff1ccb49c44b5c2ab73e9b2a92ed5056fc
Cloud User <ec2-user@ip-172-31-9-111.ap-northeast-1.compute.internal>
2016-10-14 00:19:03 +0900

    modify mysql connector

--- mysql_connector.py
+++ mysql_connector.py
@@ -2,27 +2,26 @@ import MySQLdb
 
 class mysqlConnector:
 
-	def __init__(self):
+    def __init__(self):
         self.hostname = 'localhost'
         self.dbname = 'oanda_db'
         self.username = 'trade_user'
         self.password = 'trade_user'
         self.character = 'utf8'
-        self.connector = MySQLdb.connect(host=hostname, db=dbname, user=username, passwd=password, charset=character)
-        self.cursor = connector.cursor()
+        self.connector = MySQLdb.connect(host=self.hostname, db=self.dbname, user=self.username, passwd=self.password, charset=self.character)
+        self.cursor = self.connector.cursor()
 
-    def insert_price(instrument, ask_price, bid_price):
-        sql = u"insert into price_table(instrument, ask_price, bid_price) values(%s, %s, %s)" % (instrument, ask_price, bid_price)
-        cursor.execute(sql)
-        connector.commit()
+    def insert_price(self, instrument, ask_price, bid_price):
+        sql = u"insert into price_table(instrument, ask_price, bid_price) values('%s', %s, %s)" % (instrument, ask_price, bid_price)
+        self.cursor.execute(sql)
+        self.connector.commit()
 
-    def select_price():
-	    results = []
-	    sql = u"select instrument, ask_price, bid_price from price_table order by id desc limit 60;"
-	    cursor.execute(sql)
-	    responses = cursor.fetchall()
-	    for response in responses:
-	        results.append(response) 
-	        
-    	return response    
+    def select_price(self):
+        results = []
+        sql = u"select instrument, ask_price, bid_price from price_table order by id desc limit 60;"
+        self.cursor.execute(sql)
+        responses = self.cursor.fetchall()
+        for response in responses:
+            results.append(response) 
+    	return results  
 
