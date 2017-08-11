0bbd4964712ee4fa4293e8cf8ebf7c49c7f48589
tomoyanp <tomoyanpy@gmail.com>
2017-08-11 17:49:39 +0900

    commit

--- mysql_connector.py
+++ mysql_connector.py
@@ -1,27 +1,22 @@
 import MySQLdb
 
-class mysqlConnector:
+class MysqlConnector:
 
     def __init__(self):
         self.hostname = 'localhost'
         self.dbname = 'oanda_db'
-        self.username = 'trade_user'
-        self.password = 'trade_user'
+        self.username = 'tomoyan'
+        self.password = 'tomoyan180'
         self.character = 'utf8'
         self.connector = MySQLdb.connect(host=self.hostname, db=self.dbname, user=self.username, passwd=self.password, charset=self.character)
         self.cursor = self.connector.cursor()
-
-    def insert_price(self, instrument, ask_price, bid_price):
-        sql = u"insert into price_table(instrument, ask_price, bid_price) values('%s', %s, %s)" % (instrument, ask_price, bid_price)
+    
+    def insert_sql(self, sql):
         self.cursor.execute(sql)
         self.connector.commit()
 
-    def select_price(self, sql):
-        limit = time * 6
-        results = []
+    def select_sql(self, sql):
         self.cursor.execute(sql)
-        responses = self.cursor.fetchall()
-        for response in responses:
-            results.append(response) 
-    	return results  
+        response = self.cursor.fetchall()
+        return response
 
