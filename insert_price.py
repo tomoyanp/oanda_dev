931a1e1575e0b0b2b2d6906c840dd8e02fef8964
tomoyan <tomoyanpy@gmail.com>
2017-08-11 18:53:17 +0900

    create insert price program

--- insert_price.py
+++ insert_price.py
@@ -1,55 +1,49 @@
 # coding: utf-8
 from mysql_connector import MysqlConnector
 
+# 実行スクリプトのパスを取得して、追加
 import sys
 import os
-
-# 実行スクリプトのパスを取得して、追加
 current_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(current_path)
 sys.path.append(current_path + "/trade_algorithm")
 sys.path.append(current_path + "/obj")
 
-from datetime import datetime, timedelta
-from start_end_algo import StartEndAlgo
 from price_obj import PriceObj
-from order_obj import OrderObj
-import oandapy
 import time
-
-
 
 account_id = 2542764
 token = '85abe6d9c2646b9c56fbf01f0478a511-fe9cb897da06cd6219fde9b4c2052055'
 oanda = oandapy.API(environment="practice", access_token=token)
-price_list = []
 
-def get_price(currency):
-    response = oanda.get_prices(instruments=currency)
-    prices = response.get("prices")
-    price_time = prices[0].get("time")
-    instrument = prices[0].get("instrument")
-    asking_price = prices[0].get("ask")
-    selling_price = prices[0].get("bid")
-    price_obj = PriceObj(instrument, price_time, asking_price, selling_price)
-    return price_obj
+if __name__ == "__main__":
+    con = MysqlConnector()
+    currency = "GBP_JPY"
+    polling_time = 1
+    sleep_time = 3600
 
+    price_obj = get_price(currency)
+    older_ask_price = price_obj.getAskingPrice()
 
+    while True:
+        price_obj = get_price(currency)
+        ask_price = price_obj.getAskingPrice()
+        bid_price = price_obj.getSellingPrice()
 
-con = MysqlConnector()
-currency = "GBP_JPY"
-price_obj = get_price(currency)
-ask_price = price_obj.getAskingPrice()
-bid_price = price_obj.getSellingPrice()
+        # 前回値と同じだった場合、休日のためスリープする
+        if ask_price == older_ask_price:
+            time.sleep(sleep_time)
 
-sql = u"insert into GBP_JPY_TABLE(ask_price, bid_price) values(%s, %s)" % (ask_price, bid_price)
+        else:
+            sql = u"insert into %s_TABLE(ask_price, bid_price) values(%s, %s)" % (currency, ask_price, bid_price)
+            con.insert_sql(sql)
 
+        older_ask_price = ask_price
+        time.sleep(polling_time)
 
-con.insert_sql(sql)
-sql = u"select * from GBP_JPY_TABLE"
-response = con.select_sql(sql)
-
-for line in response:
-    print type(line)
-    for obj in line:
-        print obj    
+#    sql = u"select * from GBP_JPY_TABLE"
+#    response = con.select_sql(sql)
+#    for line in response:
+#        print type(line)
+#        for obj in line:
+#            print obj
---
+++ oanda_wrapper.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import oandapy
+from price_obj import PriceObj
+
+Class OandaWrapper:
+    def __init__(self, env, account_id, token):
+        self.oanda = oandapy.API(environment=env, access_token=token)
+
+    def get_price(currency):
+        response = self.oanda.get_prices(instruments=currency)
+        prices = response.get("prices")
+        price_time = prices[0].get("time")
+        instrument = prices[0].get("instrument")
+        asking_price = prices[0].get("ask")
+        selling_price = prices[0].get("bid")
+        price_obj = PriceObj(instrument, price_time, asking_price, selling_price)
+        return price_obj
