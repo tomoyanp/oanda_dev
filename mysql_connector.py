daff61c615db56ab377476debf9da71179c9562e
tomoyan <tomoyanpy@gmail.com>
2017-08-11 17:52:03 +0900

    conflict modify

--- mysql_connector.py
+++ mysql_connector.py
@@ -10,7 +10,7 @@ class MysqlConnector:
         self.character = 'utf8'
         self.connector = MySQLdb.connect(host=self.hostname, db=self.dbname, user=self.username, passwd=self.password, charset=self.character)
         self.cursor = self.connector.cursor()
-    
+
     def insert_sql(self, sql):
         self.cursor.execute(sql)
         self.connector.commit()
@@ -19,4 +19,3 @@ class MysqlConnector:
         self.cursor.execute(sql)
         response = self.cursor.fetchall()
         return response
-
