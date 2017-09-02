97befdce1c1be698d564e37e159da4c2fb64ccb6
tomoyanp <tomoyanpy@gmail.com>
2017-09-02 19:15:23 +0900

    complete send mail

--- send_mail.py
+++ send_mail.py
@@ -8,7 +8,7 @@ class SendMail:
     self.from_address = from_address
     self.to_address = to_address
     self.jp = 'iso-2022-jp'
-    self.sender = smtplib.SMTP('172.94.253.71')
+    self.sender = smtplib.SMTP('smtp.gmail.com')
 
   def set_msg(self, msg):
     self.msg = msg
@@ -17,6 +17,8 @@ class SendMail:
     self.subject = subject
 
   def send_mail(self):
+    self.sender.starttls()
+    self.sender.login('tomoyanpy@gmail.com', 'tomoyan180')
     send_msg = MIMEText(self.msg.encode(self.jp), 'plain', self.jp,)
     send_msg['Subject'] = self.subject
     send_msg['From'] = self.from_address
