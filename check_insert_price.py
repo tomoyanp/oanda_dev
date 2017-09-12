f6a134aeb46790ec70b295df74e369589bcfc0cf
tomoyan <tomoyanpy@gmail.com>
2017-09-12 18:28:40 +0900

    modify

--- check_insert_price.py
+++ check_insert_price.py
@@ -3,12 +3,12 @@
 import commands
 import time
 
-status = commands.getoutput("ps -ef | grep python | grep insert_price.py | grep -v check | wc -l")
 
 while True:
+    status = commands.getoutput("ps -ef | grep python | grep insert_price.py | grep -v check | wc -l")
     if status == 1:
         pass
     else:
         commands.getoutput("/bin/bash restart_insert_price.sh")
-    
+
     time.sleep(5)
