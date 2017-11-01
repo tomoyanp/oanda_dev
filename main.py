15c4bb82912bfe2cc3cf2ff804e88aaa78256990
tomoyan <tomoyanpy@gmail.com>
2017-11-01 20:33:17 +0900

    modi

--- main.py
+++ main.py
@@ -64,9 +64,13 @@ if __name__ == '__main__':
               time.sleep(polling_time)
               base_time = datetime.now()
 
-          trade_wrapper.setInstrumentRespoonse(base_time)
-          trade_wrapper.tradeDecisionWrapper(base_time)
-          test_return_index = trade_wrapper.stlDecisionWrapper()
+          week = base_time.weekday()
+          if week == 5 or week == 6:
+              pass
+          else:
+              trade_wrapper.setInstrumentRespoonse(base_time)
+              trade_wrapper.tradeDecisionWrapper(base_time)
+              test_return_index = trade_wrapper.stlDecisionWrapper()
 
           if test_mode:
               now = datetime.now()
