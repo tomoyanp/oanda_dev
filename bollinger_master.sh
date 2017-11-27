737f148866a47069c6add11c66716be59b0fd399
tomoyanp <tomoyanpy@gmail.com>
2017-11-27 19:14:08 +0900

    modi

---
+++ bollinger_master.sh
@@ -0,0 +1,1 @@
+su - tomoyan -c 'nohup python /home/tomoyan/production/oanda_dev/main.py "USD_JPY" "production" "bollinger" "bollinger_master" > /home/tomoyan/production/stdout/bollinger_master.out &'
---
+++ timetrend_master.sh
@@ -0,0 +1,1 @@
+su - tomoyan -c 'nohup python /home/tomoyan/production/oanda_dev/main.py "USD_JPY" "production" "timetrend" "timetrend_master" > /home/tomoyan/production/stdout/timetrend_master.out &'
