3fbade27280288d87d31c51fcedd481da136223c
tomoyanp <tomoyanpy@gmail.com>
2017-12-10 16:01:54 +0900

    modi

---
+++ check.sh
@@ -0,0 +1,3 @@
+source /home/tomoyan/python-2.7-env/bin/activate
+nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "USD_JPY" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "GBP_JPY" > /dev/null &
--- start_all.sh
+++ start_all.sh
@@ -1,12 +1,9 @@
-#source /home/tomoyan/python-2.7-env/bin/activate
-#nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "USD_JPY" > /dev/null &
-#sleep 10
-#nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "GBP_JPY" > /dev/null &
-#sleep 10
-nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "GBP_JPY" > /dev/null &
-sleep 10
-nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "USD_JPY" > /dev/null &
-sleep 10
-#nohup python /home/tomoyan/staging/oanda_dev/main.py "USD_JPY" "production" "bollinger" "bollinger_master" > /dev/null &
-#sleep 10
+source /home/tomoyan/python-2.7-env/bin/activate
+nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "USD_JPY" > /dev/null &
+sleep 60
+nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "GBP_JPY" > /dev/null &
+sleep 60
+nohup python /home/tomoyan/staging/oanda_dev/main.py "USD_JPY" "production" "bollinger" "bollinger_master" > /dev/null &
+sleep 60
+nohup python /home/tomoyan/staging/oanda_dev/main.py "USD_JPY" "production" "startend" "startend_master" > /dev/null &
 #nohup python /home/tomoyan/staging/oanda_dev/main.py "USD_JPY" "production" "timetrend" "timetrend_master" > timetrend.out &
