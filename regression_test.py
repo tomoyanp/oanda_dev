4ab23eaa6fc18e0c0b6d14f69fb9556292f5ce2c
tomoyanp <tomoyanpy@gmail.com>
2017-12-25 17:24:31 +0900

    modi

--- regression_test.py
+++ regression_test.py
@@ -1,21 +1,25 @@
 #coding: utf-8
 
 import sys
+import numpy as np
+from datetime import datetime, timedelta
 
+import time
 sys.path.append("lib/")
 from mysql_connector import MysqlConnector
 
 target_time = "2017-12-01 00:00:00"
 target_time = datetime.strptime(target_time, "%Y-%m-%d %H:%M:%S")
-mysql_connector = MysqlConnector
+mysqlConnector = MysqlConnector()
+output_file = open("regression_test.txt", "w")
 
 while True:
-    output_file = open("regression_test.txt", "w")
     trend_time_width = 5
     instrument = "USD_JPY"
     trend_time_width = int(trend_time_width)
     before_time = target_time - timedelta(hours=trend_time_width)
     sql = "select ask_price from %s_TABLE where insert_time > \'%s\' and insert_time < \'%s\'" % (instrument, before_time, target_time)
+    print sql
     
     response = mysqlConnector.select_sql(sql)
     
@@ -23,13 +27,13 @@ while True:
     index_list = []
     index = 1 
     for price in response:
-        tmp_price = (price_list[0] + price_list[1]) / 2
-        price_list.append(tmp_price)
+        price_list.append(price[0])
         index_list.append(index)
         index = index + 1
     
     price_list = np.array(price_list)
     index_list = np.array(index_list)
+    print price_list
     z = np.polyfit(index_list, price_list, 3)
     a, b, c, d = np.poly1d(z)
     
@@ -39,5 +43,7 @@ while True:
     now = datetime.now()
     if target_time > now:
         break
+
+    target_time = target_time + timedelta(minutes=60)
   
 output_file.close()
