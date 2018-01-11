5b969ceccb6cd12c16ab13ffd6bc2a418b057b9e
tomoyan <tomoyanpy@gmail.com>
2018-01-11 12:02:41 +0900

    modi

--- check_insert_price.py
+++ check_insert_price.py
@@ -14,41 +14,72 @@ from oanda_wrapper import OandaWrapper
 from price_obj import PriceObj
 from datetime import datetime, timedelta
 from common import decideMarket
+from send_mail import SendMail
 import time
+
+
+
+# if check value is empty, insert record before 1 seconds 
+def follow_record(con, base_time, currency):
+    #print "value is empty, base_time = %s" % base_time
+    print("value is empty, base_time = %s" % base_time)
+    base_time_bef = base_time - timedelta(seconds=1)
+    sql = "select ask_price, bid_price from %s_TABLE where insert_time = \'%s\'" % (currency, base_time_bef)
+    #print sql
+    print(sql)
+    response = con.select_sql(sql)
+
+    ask_price = response[0][0]
+    bid_price = response[0][1]
+
+    sql = "insert into %s_TABLE (ask_price, bid_price, insert_time) values (%s, %s, \'%s\')" % (currency, ask_price, bid_price, base_time)
+    #print sql
+    print(sql)
+    con.insert_sql(sql)
 
 if __name__ == "__main__":
     args = sys.argv
     currency = args[1].strip()
     con = MysqlConnector()
-    polling_time = 60
+
+    now = datetime.now()
+    base_time = now
+
+    # for TEST
+    base_time = datetime.strptime("2018-01-11 11:00:00", "%Y-%m-%d %H:%M:%S")
 
     try:
         while True:
-            now = datetime.now()
             flag = decideMarket(now)
+
+            now = datetime.now()
+            tmp_time = now - timedelta(seconds=10)
+            if tmp_time < base_time:
+                flag = False
+                #print "base_time > now, base_time = %s, now = %s" % (base_time, now)
+                print("base_time > now, base_time = %s, now = %s" % (base_time, now))
+            else:
+                base_time = base_time + timedelta(seconds=1)
+                #print "base_time = %s" % base_time
+                print("base_time = %s" % base_time)
 
             if flag == False:
                 pass
             else:
-                start_time = now - timedelta(minutes=1)
-                start_time = start_time.strftime("%Y-%m-%d %H:%M:%S")
-                end_time   = now.strftime("%Y-%m-%d %H:%M:%S")
-
-                sql = u"select insert_time from %s_TABLE where insert_time > \'%s\' and insert_time < \'%s\'" % (currency, start_time, end_time)
-                print sql
+                sql = u"select insert_time from %s_TABLE where insert_time = \'%s\'" % (currency, base_time)
+                #print sql
+                print(sql)
                 response = con.select_sql(sql)
-                print response
-                print "==============================================="
-                for res in response:
-                    tm = res[0].strftime("%Y-%m-%d %H:%M:%S")
-                    print tm
-
-                if len(response) < 50:
-                    raise ValueError("%s_TABLE CHECK FAIL. LIST LENGTH=%s" %(currency, len(response)))
+                if len(response) == 0:
+                    follow_record(con, base_time, currency)
                 else:
                     pass
 
-                time.sleep(polling_time)
-
     except Exception as e:
-        print e.args
+        messege = "*** insert_check.py %s is Failed ***\n" % currency
+        message = message + traceback.format_exc()
+#        sendmail = SendMail("tomoyanpy@gmail.com", "tomoyanpy@softbank.ne.jp", property_path)
+#        sendmail.set_msg(message)
+#        sendmail.send_mail()
+        #print message
+        print(message)
