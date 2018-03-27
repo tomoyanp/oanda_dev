5407f1844aa18e489c8bba2fd2c37334df5719a0
tomoyanp <tomoyanpy@gmail.com>
2018-03-27 22:38:51 +0900

    modi

---
+++ tool/init.sh
@@ -0,0 +1,7 @@
+mkdir ../onfile
+mkdir ../log
+mkdir ../property
+mkdir ../onfile/expantion
+mkdir ../onfile/trendfollow
+mkdir ../onfile/trendreverse
+cp /home/tomoyan/staging/oanda_dev/property/* ../property
