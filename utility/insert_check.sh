7fbd4d7388efbd97fea4aa1cc24aeffc7bdc8aec
tomoyanpy <tomoyanpy@gmail.com>
2018-03-27 17:04:46 +0900

    modi

--- utility/insert_check.sh
+++ utility/insert_check.sh
@@ -1,4 +1,2 @@
 source /home/tomoyan/python-2.7-env/bin/activate
-nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "USD_JPY" > /home/tomoyan/staging/oanda_dev/check_usd.txt &
-nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "GBP_JPY" > /home/tomoyan/staging/oanda_dev/check_gbp.txt &
-nohup python /home/tomoyan/staging/oanda_dev/check_insert_price.py "NZD_JPY" > /home/tomoyan/staging/oanda_dev/check_nzd.txt &
+nohup python /home/tomoyan/staging/oanda_dev/utility/check_insert_price.py "GBP_JPY" > /dev/null &
--- utility/insert_price.sh
+++ utility/insert_price.sh
@@ -1,7 +1,4 @@
 source /home/tomoyan/python-2.7-env/bin/activate
-#nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "USD_JPY" > /dev/null &
-#sleep 60
-nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "GBP_JPY" > /dev/null &
-nohup python /home/tomoyan/staging/oanda_dev/insert_indicator_master.py "GBP_JPY" > /dev/null &
-#sleep 60
-#nohup python /home/tomoyan/staging/oanda_dev/insert_price.py "NZD_JPY" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/utility/insert_price.py "GBP_JPY" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/utility/insert_indicator_master.py "GBP_JPY" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/utility/insert_indicator_master_1m.py "GBP_JPY" > /dev/null &
