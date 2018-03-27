ccae07f35ea695f1ddfe04f350c5739784287fdc
tomoyanp <tomoyanpy@gmail.com>
2018-03-27 16:48:25 +0900

    modi

--- utility/start_production.sh
+++ utility/start_production.sh
@@ -1,2 +1,2 @@
 source /home/tomoyan/python-2.7-env/bin/activate 
-nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "trendfollow" "trendfollow_master" > /home/tomoyan/staging/oanda_dev/production.out &
+nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "expantion" "expantion_master" > /dev/null &
