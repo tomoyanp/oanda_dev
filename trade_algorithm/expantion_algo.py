4695c4db3b603f867213b28f0f7a036ddbd74edc
tomoyanp <tomoyanpy@gmail.com>
2018-03-31 20:51:17 +0900

    modi

--- trade_algorithm/expantion_algo.py
+++ trade_algorithm/expantion_algo.py
@@ -112,14 +112,14 @@ class ExpantionAlgo(SuperAlgo):
     def decideExpantionStopLoss(self, stl_flag, current_price):
         # 損切り逆方向にタッチしたら
         order_price = self.getOrderPrice()
-#        if self.order_kind == "buy":
-#            if current_price < self.lower_sigma_5m3:
-#                stl_flag = True
-#                logging.info("EXECUTE STLEMENT at Reverse Stl mode")
-#        elif self.order_kind == "sell":
-#            if current_price > self.upper_sigma_5m3:
-#                stl_flag = True
-#                logging.info("EXECUTE STLEMENT at Reverse Stl mode")
+        if self.order_kind == "buy":
+            if current_price < self.lower_sigma_5m3:
+                stl_flag = True
+                logging.info("EXECUTE STLEMENT at Reverse Stl mode")
+        elif self.order_kind == "sell":
+            if current_price > self.upper_sigma_5m3:
+                stl_flag = True
+                logging.info("EXECUTE STLEMENT at Reverse Stl mode")
 
         stl_flag = self.decideTrailLogic(stl_flag, self.ask_price, self.bid_price, current_price, order_price)
 
@@ -129,7 +129,8 @@ class ExpantionAlgo(SuperAlgo):
         # Buy Logic at Trend Follow Mode
 
         # slopeは上を向いている場合は買いエントリしない。下を向いている場合は売りエントリしない
-        if (self.upper_sigma_1h3 - self.lower_sigma_1h3) > 1 and (self.upper_sigma_1h3 - self.lower_sigma_1h3) < 2:
+#        if (self.upper_sigma_1h3 - self.lower_sigma_1h3) > 1 and (self.upper_sigma_1h3 - self.lower_sigma_1h3) < 2:
+        if (self.upper_sigma_1h3 - self.lower_sigma_1h3) < 2:
             if current_price > (self.upper_sigma_5m3):
 #                if self.order_history != "buy" or self.profit_history != "l":
                  if 0 == 0:
