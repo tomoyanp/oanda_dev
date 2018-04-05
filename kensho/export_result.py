7f685952b8dd3b390b7b0ee4d172d2766d8662cd
tomoyanpy <tomoyanpy@gmail.com>
2018-04-05 11:30:10 +0900

    modi

---
+++ kensho/export_result.py
@@ -0,0 +1,24 @@
+import re
+import commands
+
+
+file_list = commands.getoutput("ls *.result")
+file_list = file_list.split("\n")
+
+for rf in file_list:
+  profit = 0
+
+  write_file = open("profit_export.txt", "a")
+  write_file.write("#######################\n")
+  write_file.write("# %s\n" % rf)
+  cmd = "cat %s | grep PROFIT| grep -v STL" % rf
+  out = commands.getoutput(cmd)
+  out = out.split("\n")
+
+  for line in out:
+    pf = line.split("PROFIT=")[1]
+    pf = float(pf)
+    profit = profit + pf
+
+
+  write_file.write("# PROFIT=%s\n" % profit)
