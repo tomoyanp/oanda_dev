635360d20b94b000e6f8f088a2d363841585593b
tomoyanp <tomoyanpy@gmail.com>
2018-04-08 18:44:34 +0900

    modi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+#extentions = [Extension("lib", ["*.pyx"])]
+
+setup(
+
+      name        = 'trade_wrapper',
+
+      cmdclass    = {'build_ext':build_ext},
+
+      ext_modules = cythonize("*/*.pyx")
+#      ext_modules = cythonize(extentions)
+
+      )
