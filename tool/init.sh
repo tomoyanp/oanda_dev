780ad5413a4903b8dcb81da46df9a929cd50f32e
tomoyanpy <tomoyanpy@gmail.com>
2018-04-11 09:57:52 +0900

    modi

--- tool/init.sh
+++ tool/init.sh
@@ -2,6 +2,7 @@ mkdir ../onfile
 mkdir ../log
 mkdir ../property
 mkdir ../onfile/expantion
+mkdir ../onfile/daytime
 mkdir ../onfile/trendfollow
 mkdir ../onfile/trendreverse
 cp /home/tomoyan/master_config/property/* ../property
