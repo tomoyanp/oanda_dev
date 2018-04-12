a8be869b3bd5b876fffcdafa5ea231fb08c19b67
tomoyanp <tomoyanpy@gmail.com>
2018-04-12 23:14:51 +0900

    modi

--- trade_algorithm/daytime_algo.py
+++ trade_algorithm/daytime_algo.py
@@ -92,17 +92,17 @@ class DaytimeAlgo(SuperAlgo):
         return trade_flag
 
     def getStartPrice(self, base_time):
-        weekday = base_time.weekday()
-        if int(weekday) == 0:
-            base_time = base_time - timedelta(days=3)
-        else:
-            base_time = base_time - timedelta(days=1)
         start_time = base_time.strftime("%Y-%m-%d 07:00:00")
+        width = 24 * 3600
         #start_time = base_time.strftime("%Y-%m-%d 08:00:00")
-        sql = "select ask_price, bid_price from %s_TABLE where insert_time = \'%s\'" % (self.instrument, start_time)
+        sql = "select ask_price, bid_price, insert_time from %s_TABLE where insert_time < \'%s\' order by insert_time desc limit %s" % (self.instrument, start_time, width)
+        self.debug_logger.info(base_time)
+        self.debug_logger.info(sql)
         response = self.mysql_connector.select_sql(sql)
-        ask_price = response[0][0]
-        bid_price = response[0][1]
+        ask_price = response[-1][0]
+        bid_price = response[-1][1]
+        insert_time = response[-1][2]
+        self.debug_logger.info(insert_time)
 
         return ((ask_price + bid_price) / 2)
 
