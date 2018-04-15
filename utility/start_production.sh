784c261777b11ce1a1e2cbf2848ad24351c4120e
tomoyanpy <tomoyanpy@gmail.com>
2018-04-15 17:49:21 +0900

    final version

--- utility/start_production.sh
+++ utility/start_production.sh
@@ -1,2 +1,3 @@
 source /home/tomoyan/python-2.7-env/bin/activate 
 nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "expantion" "expantion_master" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "daytime" "daytime_master" > /dev/null &
