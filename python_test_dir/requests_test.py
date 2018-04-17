93be4bc9aa95d5c351383388465526727c8cc61e
tomoyanp <tomoyanpy@gmail.com>
2018-04-17 13:31:35 +0900

    modi

--- python_test_dir/requests_test.py
+++ python_test_dir/requests_test.py
@@ -2,8 +2,12 @@
 import requests
 
 instruments = 'GBP_JPY'
-account_id = 4093685
+#account_id = 4093685
+#token = 'e93bdc312be2c3e0a4a18f5718db237a-32ca3b9b94401fca447d4049ab046fad'
+
+account_id = 2542764
 token = 'e93bdc312be2c3e0a4a18f5718db237a-32ca3b9b94401fca447d4049ab046fad'
+
 url = "https://api-fxtrade.oanda.com/v3/accounts/%s/instruments" % account_id
 headers = {'Content-Type': 'application/json',
            'Authorization': 'Bearer %s' % token}
