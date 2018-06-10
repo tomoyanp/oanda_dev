7728b8c34aacd1256fb15fcc81cac8991f02e9fe
tomoyanp <tomoyanpy@gmail.com>
2018-06-10 21:11:53 +0900

    modi

--- utility/start_production_for_test.sh
+++ utility/start_production_for_test.sh
@@ -1,2 +1,2 @@
 source /home/tomoyan/virtualenv/.python2.7/bin/activate
-nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "demo" "multi_evolv" "multi_evolv_test" > /dev/null &
+nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "multi" "multi_master" > /dev/null &
