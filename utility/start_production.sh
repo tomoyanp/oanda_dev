dd1a88a6ef64fbc51e7556279ff89ba422041c01
tomoyanp <tomoyanpy@gmail.com>
2018-07-01 20:45:40 +0900

    modi

--- utility/start_production.sh
+++ utility/start_production.sh
@@ -3,4 +3,4 @@ source /home/tomoyan/virtualenv/.python2.7/bin/activate
 #nohup python /home/tomoyan/staging/oanda_dev/main.py "GBP_JPY" "production" "multi" "multi_master" > /dev/null &
 #nohup python /home/tomoyan/hayata/oanda_dev/main.py "GBP_JPY" "production" "multi" "multi_master" > /dev/null &
 nohup python /home/tomoyan/staging/oanda_dev//main.py "GBP_JPY" "production" "volatility" "volatility_master" > /dev/null &
-nohup python /home/tomoyan/hayata/oanda_dev/main.py "GBP_JPY" "production" "volatility" "volatility_master" > /dev/null &
+nohup python /home/tomoyan/hayata/oanda_dev/main.py "GBP_JPY" "demo" "volatility" "volatility_master" > /dev/null &
