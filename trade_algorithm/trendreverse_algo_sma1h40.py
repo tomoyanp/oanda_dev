551821afa99d6d953d225a0f3207f1b6f2efd99d
tomoyanpy <tomoyanpy@gmail.com>
2018-07-27 18:01:04 +0900

    modi

--- trade_algorithm/trendreverse_algo_sma1h40.py
+++ trade_algorithm/trendreverse_algo_sma1h40.py
@@ -270,7 +270,7 @@ class TrendReverseAlgo(SuperAlgo):
                     if 1 == 1:
                         if self.first_trade_flag == "buy":
                             #if self.decidePerfectOrder("5m") == "sell" and self.second_trade_flag == False:
-                            if current_price < self.sma1h20 and  self.second_trade_flag == False:
+                            if current_price < self.sma1h40 and  self.second_trade_flag == False:
                                 self.debug_logger.info("second trade flag: True at %s" % base_time)
                                 self.second_trade_time = base_time
                                 self.second_trade_flag = True
@@ -278,7 +278,7 @@ class TrendReverseAlgo(SuperAlgo):
 
                         elif self.first_trade_flag == "sell":
                             #if self.decidePerfectOrder("5m") == "buy" and self.second_trade_flag == False:
-                            if current_price > self.sma1h20 and  self.second_trade_flag == False:
+                            if current_price > self.sma1h40 and  self.second_trade_flag == False:
                                 self.debug_logger.info("second trade flag: True at %s" % base_time)
                                 self.second_trade_time = base_time
                                 self.second_trade_flag = True
